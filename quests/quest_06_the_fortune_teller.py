202985f742ff83aab440223586fd0e772359e517
EnzoAsaph <129498137+EnzoAsaph@users.noreply.github.com>
2026-02-27 21:59:03 +0200

    quests completed

---
+++ quests/quest_06_the_fortune_teller.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+
+print("Hello " + name + ".")
+print("Your quest is: " + quest + ".")
---
+++ quests/quest_21_the_reusable_incantation.py
@@ -0,0 +1,6 @@
+def greet_adventurer():
+    print("Hello you are welcome")
+
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()
