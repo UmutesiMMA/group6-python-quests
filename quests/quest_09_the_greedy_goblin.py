cc6be43d5e38f043436d70690b6a85b9c6a541a2
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-02-27 16:31:03 +0200

    add quest 9

---
+++ quests/quest_09_the_greedy_goblin.py
@@ -0,0 +1,13 @@
+import math
+
+gold_pieces_num = 27
+friends_num = 4
+# Calculating the pieces that each friend will receive
+pieces_given = math.floor(gold_pieces_num / friends_num)
+
+# Calculating the pieces the goblin will remain with
+
+remaining_pieces = gold_pieces_num % pieces_given
+
+print(pieces_given)
+print(remaining_pieces)
