b57cdc08dc822e9958e2cbdba0617b21481bda57
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-02-27 13:18:52 +0200

    add answer for quest 4

--- quests/quest_04_town_crier.py
+++ quests/quest_04_town_crier.py
@@ -1,1 +1,6 @@
-# activity answer
+# level 1 - Quest 4
+cityName =  "Kigali"
+year = 2026
+name = "Augusta"
+
+print(f"Welcome to {cityName}! The year is {year}, and our newest resident is {name}.")
